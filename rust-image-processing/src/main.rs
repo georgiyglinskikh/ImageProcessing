7e05b56e136dee3ce684194f6984f6b75f37cbb6
georgiyglinskikh <georgiyglinskikh@gmail.com>
2020-12-26 12:43:06 +0500

    Make an ability to read & write picture files [RUST]

--- rust-image-processing/src/main.rs
+++ rust-image-processing/src/main.rs
@@ -1,3 +1,19 @@
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    let image_path = Path::new(&args[1]);
+
+    let mut image =
+        image::open(image_path.to_str().unwrap())
+        .expect("cannot find image");
+
+    image.save(
+        format!(
+                "{}/1_{}",
+                image_path.parent().unwrap().to_str().unwrap(),
+                image_path.file_name().unwrap().to_str().unwrap()
+            )
+        ).expect("cannot save picture");
 }
